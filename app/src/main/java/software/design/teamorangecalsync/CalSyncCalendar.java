1fa6d7fe33f996830fc10e8ca86205aaa58f572e
edivera <ediliovera97@gmail.com>
2018-04-26 20:15:05 -0500

    refactored Calendar1.java test calendar class into CalSyncCalendar.java

---
+++ app/src/main/java/software/design/teamorangecalsync/CalSyncCalendar.java
@@ -0,0 +1,9 @@
+package software.design.teamorangecalsync;
+
+public class CalSyncCalendar extends MainCalendar {
+
+    public CalSyncCalendar(String name) {
+        super(name);
+    }
+
+}
--- app/src/main/java/software/design/teamorangecalsync/Calendar1.java
+++
@@ -1,9 +0,0 @@
-package software.design.teamorangecalsync;
-
-public class Calendar1 extends MainCalendar {
-
-    public Calendar1() {
-        name = "Calendar1";
-    }
-
-}
